a95c181c2068964f3cc8017c76c5a28a1c001293
Maitrik Patel <maitrik1419@gmail.com>
2019-01-17 04:45:45 -0800

    cleanup slider

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -4,72 +4,74 @@ import PropTypes from 'prop-types'
 import { CarouselProvider, Slider, Image, Dot, Slide, ButtonBack, ButtonNext, DotGroup, ButtonPlay } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
-import BroswerFrame from '../BroswerFrame'
 import styles from './Carousel.module.css'
 
-import AviatioImage1 from './work/Aviatio/AviatioSS1.jpg'
-import AviatioImage2 from './work/Aviatio/AviatioSS2.jpg'
-import AviatioImage3 from './work/Aviatio/AviatioSS3.jpg'
-import AviatioImage4 from './work/Aviatio/AviatioSS4.jpg'
+import AviatioImageOne from './work/Aviatio/AviatioSS1.jpg'
+import AviatioImageTwo from './work/Aviatio/AviatioSS2.jpg'
+import AviatioImageThree from './work/Aviatio/AviatioSS3.jpg'
+import AviatioImageFour from './work/Aviatio/AviatioSS4.jpg'
 
 function Carousel(props) {
-  const { variant, ...restProps } = props
 
-  // height : Work.height
-  // width : Work.width
-  // TotalSlides : Work.images.length
-  // Src : Work.images { image.src }
-  // Alt : Work.images { image.text }
-  // Image Name : Work.images { image.text }
-
-  // { 
-  //   Sony: {
-  //     width: ""
-  //     height: ""
-  //     Images: {
-  //       image1: {
-  //         src: ""
-  //         text: ""
-  //       }
-  //     }
-  //   }
-  //   DocuSign: {
-  //     width: ""
-  //     height: ""
-  //     Images: {
-  //       image1: {
-  //         src: ""
-  //         text: ""
-  //       }
-  //     }
-  //   }
-  // }
+  const { 
+    variant,
+    ...restProps 
+  } = props
   
+  const tempObject = {
+    Aviatio: {
+      width: "1024",
+      height: "670",
+      Images: [
+        {
+          SrcUrl: AviatioImageOne,
+          Text: "AviatioImageOne",
+        },
+        {
+          SrcUrl: AviatioImageTwo,
+          Text: "AviatioImageTwo"
+        },
+        {
+          SrcUrl: AviatioImageThree,
+          Text: "AviatioImageThree"
+        },
+        {
+          SrcUrl: AviatioImageFour,
+          Text: "AviatioImageFour"
+        }
+      ]
+    }
+  }
+
+  console.log(variant)
+
+  const Slides = tempObject.Aviatio.Images.map((image, index) =>
+    <Slide 
+      innerClassName={styles.Slide} 
+      index={index}
+      key={index}
+      >
+        <Image src={image.SrcUrl} alt={image.Text} />
+        <p className={styles.SlideText}>{image.Text}</p>
+    </Slide>
+  );
+
   return (
     <div className={styles.Carousel}>
-      <h2>Variant Type : {variant}</h2>
+      <h3 className={styles.CarouselTitle}>{variant} Slider</h3>
       <CarouselProvider
-        naturalSlideWidth={1024}
-        naturalSlideHeight={670}
-        totalSlides={4}
+        naturalSlideWidth={tempObject.Aviatio.width}
+        naturalSlideHeight={tempObject.Aviatio.height}
+        totalSlides={tempObject.Aviatio.Images.length}
         isPlaying
         interval='4000'
       >        
         <Slider className={styles.Slider}>
-          <Slide innerClassName={styles.Slide} index={0}><Image src={AviatioImage1} alt=""></Image><p className={styles.SlideText}>Image Name : 1</p></Slide>
-          <Slide innerClassName={styles.Slide} index={1}><Image src={AviatioImage2} alt=""></Image><p className={styles.SlideText}>Image Name : 2</p></Slide>
-          <Slide innerClassName={styles.Slide} index={2}><Image src={AviatioImage3} alt=""></Image><p className={styles.SlideText}>Image Name : 3</p></Slide>
-          <Slide innerClassName={styles.Slide} index={3}><Image src={AviatioImage4} alt=""></Image><p className={styles.SlideText}>Image Name : 4</p></Slide>
+          {Slides}
         </Slider>
+
         <div className={styles.SliderActions}>
           <DotGroup className={styles.Dots}/>
-          {/* <div className={styles.Dots}>
-            <Dot className={styles.Dot} slide={0}><p className={styles.SlideText}>Image Name : 1</p></Dot>
-            <Dot className={styles.Dot} slide={1}><p className={styles.SlideText}>Image Name : 2</p></Dot>
-            <Dot className={styles.Dot} slide={2}><p className={styles.SlideText}>Image Name : 3</p></Dot>
-            <Dot className={styles.Dot} slide={3}><p className={styles.SlideText}>Image Name : 4</p></Dot>
-          </div> */}
-
           <div className={styles.SliderControls}>
             <ButtonBack className={styles.Back} />
             <ButtonPlay 
@@ -79,8 +81,8 @@ function Carousel(props) {
             />
             <ButtonNext className={styles.Next} />
           </div>
-
         </div>
+
       </CarouselProvider>
     </div>
   )
