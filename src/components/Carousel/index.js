01cf6b51c67a71ac4f3f16a4fabd46902dcff3cc
Maitrik Patel <maitrik1419@gmail.com>
2019-01-17 02:05:58 -0800

    slider style

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -5,11 +5,12 @@ import { CarouselProvider, Slider, Image, Slide, ButtonBack, ButtonNext, DotGrou
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import BroswerFrame from '../BroswerFrame'
+import styles from './Carousel.module.css'
+
 import AviatioImage1 from './work/Aviatio/AviatioSS1.jpg'
 import AviatioImage2 from './work/Aviatio/AviatioSS2.jpg'
 import AviatioImage3 from './work/Aviatio/AviatioSS3.jpg'
 import AviatioImage4 from './work/Aviatio/AviatioSS4.jpg'
-import AviatioImage5 from './work/Aviatio/AviatioSS5.jpg'
 
 function Carousel(props) {
   const { variant, ...restProps } = props
@@ -44,30 +45,36 @@ function Carousel(props) {
   //   }
   // }
   
-  
   return (
     <React.Fragment>
       <h2>Variant Type : {variant}</h2>
         <CarouselProvider
           naturalSlideWidth={1024}
           naturalSlideHeight={670}
-          totalSlides={5}
+          totalSlides={4}
           isPlaying
-          interval='2000'
+          interval='2500'
         >        
-          <BroswerFrame>
-            <Slider>
-              <Slide index={0}><Image src={AviatioImage1} alt=""></Image><p>Image Name : 1</p></Slide>
-              <Slide index={1}><Image src={AviatioImage2} alt=""></Image><p>Image Name : 2</p></Slide>
-              <Slide index={2}><Image src={AviatioImage3} alt=""></Image><p>Image Name : 3</p></Slide>
-              <Slide index={3}><Image src={AviatioImage4} alt=""></Image><p>Image Name : 4</p></Slide>
-              <Slide index={4}><Image src={AviatioImage5} alt=""></Image><p>Image Name : 5</p></Slide>
+          {/* <BroswerFrame> */}
+            <Slider className={styles.Slider}>
+              <Slide innerClassName={styles.Slide} index={1}><Image src={AviatioImage1} alt=""></Image><p className={styles.SlideText}>Image Name : 1</p></Slide>
+              <Slide innerClassName={styles.Slide} index={2}><Image src={AviatioImage2} alt=""></Image><p className={styles.SlideText}>Image Name : 2</p></Slide>
+              <Slide innerClassName={styles.Slide} index={3}><Image src={AviatioImage3} alt=""></Image><p className={styles.SlideText}>Image Name : 3</p></Slide>
+              <Slide innerClassName={styles.Slide} index={4}><Image src={AviatioImage4} alt=""></Image><p className={styles.SlideText}>Image Name : 4</p></Slide>
             </Slider>
-          </BroswerFrame>
-          <DotGroup />
-          <ButtonBack>&#129120;</ButtonBack>
-          <ButtonNext>&#129122;</ButtonNext>
-          <ButtonPlay childrenPlaying="&#9632;" childrenPaused="&#9654;"><p></p></ButtonPlay> 
+          {/* </BroswerFrame> */}
+          <div>
+            <DotGroup className={styles.Dots}/>
+            <div className={styles.SliderControls}>
+              <ButtonBack className={styles.Back} />
+              <ButtonPlay 
+                childrenPlaying="&#9632;" 
+                childrenPaused="&#9654;" 
+                className={styles.PlayPause}>
+              </ButtonPlay> 
+              <ButtonNext className={styles.Next} />
+            </div>
+          </div>
         </CarouselProvider>
     </React.Fragment>
   )
