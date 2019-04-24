eb07f6b21dd252ed9cdb6afe7377988f4ec721bd
Maitrik Patel <maitrik1419@gmail.com>
2019-04-24 01:13:28 -0700

    fix typo

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -18,7 +18,7 @@ function SEO(props) {
         { name: 'description', content: siteInfo.siteDescription },
         { name: 'keywords', content: siteInfo.toolsList.concat(siteInfo.skillsList) },
         { name: "image", content:siteImage},
-        { name: 'google-site-verification', content: 'PoixIz8vjc-yvhRCJWhiWPFUei1BCSj0pN28g2NZR9M' }
+        { name: 'google-site-verification', content: 'PoixIz8vjc-yvhRCJWhiWPFUei1BCSj0pN28g2NZR9M' },
 
         // Google / Search Engine Tags
         { itemprop:"name" , content: siteTitle },
